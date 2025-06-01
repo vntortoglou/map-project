5731faa6e06e784bf25a420ec0b47dc70d2e6e84
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-06-01 15:28:15 +0300

    added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
+      <div className="App">
+        <h1>Map Project Makis</h1>
+        <p>Welcome to the Map Project!</p>
+      </div>
 
+      <footer className="footer">
+        <p>© 2025 Map Project. All rights reserved.</p>
+      </footer>
     </>
   )
 }
