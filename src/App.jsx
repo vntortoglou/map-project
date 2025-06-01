c158de43aa8a10e1220d9dabbea669b4390a5f9e
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-06-01 15:18:31 +0300

    added leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import './App.css';
-
-// Για να διορθωθεί το default marker icon στο Leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-});
+import './App.css'
 
 function App() {
+
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <MapContainer center={[37.9838, 23.7275]} zoom={13} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[37.9838, 23.7275]}>
-          <Popup>
-            Εδώ είναι η Αθήνα! 🇬🇷
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+    <>
+
+    </>
+  )
 }
 
-export default App;
+export default App
