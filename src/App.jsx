557ad760aeac8b9f1539f1d8a0ddd4ab5e7348fa
Vangelis <vntortoglou@gmail.com>
2025-06-10 22:36:19 +0300

    Added Map with Leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+
+
+
+
 
 function App() {
-
   return (
-    <>
-      <div className="App">
-        <h1>Map Project Makis</h1>
-        <p>Welcome to the Map Project!</p>
-      </div>
+    <div style={{ height: '100vh' }}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: '100%', width: '100%' }}
+      >
+<TileLayer
+  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+  attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
+/>
 
-      <footer className="footer">
-        <p>© 2025 Map Project. All rights reserved.</p>
-      </footer>
-    </>
-  )
+
+
+
+
+
+
+
+
+
+      </MapContainer>
+    </div>
+  );
 }
 
-export default App
+
+export default App;
