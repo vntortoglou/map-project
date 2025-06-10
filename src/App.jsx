744af553c9de10c3bfb939f285f1ec93497f40bb
Vangelis <vntortoglou@gmail.com>
2025-06-10 23:18:56 +0300

    Added Default location with Geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,48 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { useEffect } from 'react';
-import 'leaflet/dist/leaflet.css';
-
-
-
-
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+
+  // Code to get default Position with Geolocation
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const { latitude, longitude } = pos.coords;
+        setPosition([latitude, longitude]);
+      },
+      (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        setPosition([40.6401, 22.9444]);
+      },
+      options
+    );
+  }, []);
+
   return (
-    <div style={{ height: '100vh' }}>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{ height: '100%', width: '100%' }}
-      >
-<TileLayer
-  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-  attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
-/>
-
-
-
-
-
-
-
-
-
-
-      </MapContainer>
+    <div style={{ height: "100vh" }}>
+      {position && (
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
+          />
+        </MapContainer>
+      )}
     </div>
   );
 }
-
 
 export default App;
