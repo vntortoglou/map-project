9a401c1d72a16cc42747ad6dc8cefcb932065767
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-06-14 15:51:47 +0300

    changing the Searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,18 +5,24 @@ export default function SearchBar({ handleSearch }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleSearch(cityName);
+    if (cityName.trim() === "") {
+      handleSearch(cityName); // Default city if input is empty
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <h3>SearchBar</h3>
-        <input 
-          value={cityName}
-          onChange={(e) => setCityName(e.target.value)}
-        />
-        <button type="submit">Search</button>
+        <label className="text-2xl font-bold mb-2">Search City</label>
+        <div className="border-1 rounded p-2 mb-4">
+          <input 
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            className="w-80%"
+            placeholder="Enter city name"
+          />
+          <button type="submit" aria-label="Search" className="pr-2">🔍</button>
+        </div>
       </form>
     </div>
   );
